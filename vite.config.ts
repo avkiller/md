2871a09dac8885ec5dd34062ca13d93ac9c9e0cd
avkiller <avkiller2012@gmail.com>
2025-01-03 22:41:34 +0800

    disable external juice time cost more

--- vite.config.ts
+++ vite.config.ts
@@ -23,12 +23,12 @@ export default defineConfig({
     // sourcemap: true,
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: [`juice`],
-      plugins: [
-        externalGlobals({
-          juice: "juice"
-        })
-      ],
+      // external: [`juice`],
+      // plugins: [
+      //   externalGlobals({
+      //     juice: "juice"
+      //   })
+      // ],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         manualChunks: {
